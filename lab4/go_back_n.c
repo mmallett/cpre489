dad72e45e6f6810d78b7e4ea9c9c37f585d9c9c3
Mathew Mallett <mmallett@iastate.edu>
2013-10-03 16:51:41 -0500

    thread init added to main

--- lab4/go_back_n.c
+++ lab4/go_back_n.c
@@ -6,3 +6,37 @@
  */
 
 #include "go_back_n.c"
+
+int main(){
+
+
+	//init thread stuff
+	pthread_t sender_tid;
+	pthread_t receiver_tid;
+
+	pthread_mutex_init(&send_to_receive_mut, NULL);
+	pthread_mutex_init(&receive_to_send_mut, NULL);
+
+	pthread_cond_init(&sender_cv, NULL);
+	pthread_cond_init(&receiver_cv, NULL);
+
+	pthread_create(&sender_tid, NULL, sender, NULL);
+	pthread_create(&receiver_tid, NULL, receiver, NULL);
+
+	//join the 2 threads
+	pthread_join(&sender_tid, NULL);
+	pthread_join(&receiver_tid, NULL);
+
+	return 0;
+}
+
+void *sender(void* arg){
+
+
+}
+
+void *receiver(void* arg){
+
+
+
+}
