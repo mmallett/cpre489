39895d6b096a55ae498a0004f56a14c9fb891826
Mathew Mallett <mmallett@iastate.edu>
2013-10-03 16:23:46 -0500

    add go back n files

---
+++ lab4/go_back_n.c
@@ -0,0 +1,6 @@
+/**
+ * Matt Mallett mmallett@iastate.edu
+ * Adam Sunderman asu1351@iastate.edu
+ * go_back_n.c
+ * go back n ARQ protocol implementation
+ */
---
+++ lab4/go_back_n.h
@@ -0,0 +1,7 @@
+/**
+ * Matt Mallett mmallett@iastate.edu
+ * Adam Sunderman asu1351@iastate.edu
+ * go_back_n.h
+ * go back n ARQ definitions
+ */
+
